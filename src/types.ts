8cad19226027c2a083b9d5b3c3ef709bfb8d8d7c
umammm <148299462+Umammm3@users.noreply.github.com>
2026-04-13 21:13:26 +0700

    Add files via upload

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export interface Item {
+  id: string;
+  code: string;
+  name: string;
+  date: string;
+  description: string;
+  createdAt: number;
+}
+
+export interface Module {
+  id: string;
+  title: string;
+  link: string;
+  createdAt: number;
+}
